4cd88144b0d6d14b8e70817dd93b1dfb937d6bad
EmilEinarsen <47479113+EmilEinarsen@users.noreply.github.com>
2020-10-01 19:51:08 +0200

    Improved structure and added a (nonoptional) initial function call to the throttle method, which resets on idle.

--- Restrain.js
+++ Restrain.js
@@ -1,62 +1,75 @@
-export function Restrain() {
-	let timer
+export default function Restrain() {
+
+	let debounceTimer
 	this.debounce = (func, delay, cancel) => {
 		return new Promise((resolve, reject) => {
 			
-			clearTimeout(timer)
+			clearTimeout(debounceTimer)
 			if(cancel) reject('function canceled')
 			
-			timer = setTimeout(() => {
+			debounceTimer = setTimeout(() => {
 				func()
 				resolve('function fired')
 			}, delay )
 
 		})
 	}
+
+	
+	let throttleTimer
+	let init = true
 	this.throttle = (func, delay, cancel) => {
 		return new Promise(resolve => {
 
-			if(cancel) timer = undefined
-			if(timer) return
-
-			timer = setTimeout(() => {
+			if(cancel) throttleTimer = undefined
+			if(throttleTimer) return
+			if(init) {
+				init = false
 				func()
-				timer = undefined
+				resolve('function fired')
+			}
+			this.debounce(() => init = true, delay)
+
+			throttleTimer = setTimeout(() => {
+				func()
+				throttleTimer = undefined
 				resolve('function fired')
 			}, delay)
-
 		})
 	}
 }
 
-/* 
-debounceTest()
+
 function debounceTest() {
 	let index = 0
 	let interval
 	const { debounce } = new Restrain()
+
 	interval = setInterval(async() => {
+
 		index++
 		console.log(index)
+
 		debounce(() => {
 			console.log('first fire')
 		}, 1000 ).then(()=>console.log('then fire'))
 
 		if(index === 50) clearInterval(interval)
 	}, 10)
-} */
-
-
-/* throttleTest()
+}
 async function throttleTest() {
 	let index = 0
 	let interval
 	const { throttle } = new Restrain()
+
 	interval = setInterval(async() => {
+
 		if(index === 100) clearInterval(interval)
 		index++
 		console.log(index)
+
 		const t = await throttle(() => {}, 1000)
+
 		console.log(t)
 	}, 50)
-} */
+}
