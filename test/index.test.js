ef10b165d433422c169da35f1b7864197defc8f6
EmilEinarsen <47479113+EmilEinarsen@users.noreply.github.com>
2020-10-02 21:17:51 +0200

    added dir test to public

---
+++ test/index.test.js
@@ -0,0 +1,40 @@
+const Restrain = require('../index')
+
+
+test('Tested Restrain', () => {
+	expect('').toBe('')
+})
+
+
+function debounceTest() {
+	let interval, index = 0
+	const { debounce } = new Restrain()
+
+	interval = setInterval(async() => {
+		console.log(index++)
+
+		/**
+		 * After not being called for delayed-duration, execute given function
+		 */
+		debounce(() => console.log('first execution'), 1000 )
+
+		if(index === 20) clearInterval(interval)
+	}, 100)
+}
+
+throttleTest()
+async function throttleTest() {
+	let index = 0
+	let interval
+	const { throttle } = new Restrain()
+
+	interval = setInterval(async() => {
+		if(index === 20) clearInterval(interval)
+		console.log(index++)
+
+		/**
+		 * After delayed-duration, execute given function
+		 */
+		throttle(() => console.log('execution'), 1000)
+	}, 200)
+}
