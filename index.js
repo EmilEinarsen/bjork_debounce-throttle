467b6bbff562f35f0a988b162ef6f847b711f702
EmilEinarsen <47479113+EmilEinarsen@users.noreply.github.com>
2020-10-02 20:28:57 +0200

    branch for throttle development

--- index.js
+++ index.js
@@ -42,15 +42,38 @@ function Restrain() {
 	 * @property {number} throttleTimer
 	 */
 	let throttleTimer
+	let firstCall = true
+	let counter = 0
+	/**
+	 * Optional params for additional configuration of throttling
+	 * @typedef {Object} Options
+	 * @property {boolean} [cancel]
+	 * @property {boolean} [init]
+	 * @property {number} [executeOnCount]
+	 */
 	/**
 	 * Restrain execution of **func** too one every duration of **delay**
+	 * @function throttle
 	 * @param {function} func - callback function
 	 * @param {number} delay - ms
-	 * @param {bool} cancel - cancels current timeout, allowing a new timeout to replace it
-	 * @returns {Promise} - resolve on execution
+	 * @param {Options} options
 	 */
-	this.throttle = (func, delay, cancel) => {
-		return new Promise((resolve, reject) => {
+	this.throttle = (func, delay, options = {cancel = false, init = true, executeOnCount = 0} = {}) => {
+		let {cancel, init, executeOnCount} = options
+		return new Promise((resolve) => {
+			
+			if(firstCall && init) {
+				func()
+				firstCall = false
+			}
+			if(init) this.debounce(() => firstCall = true, delay*1.5)
+
+			counter++
+			if(executeOnCount === counter) {
+				func()
+				counter = 0
+				resolve('function executed')
+			}
 
 			// cancels current timeout, allowing a new timeout to replace it
 			if(cancel) {
@@ -69,6 +92,25 @@ function Restrain() {
 			}, delay)
 		})
 	}
+}
+
+throttleTest()
+async function throttleTest() {
+	let index = 0
+	let interval
+	const { throttle } = new Restrain()
+
+	interval = setInterval(async() => {
+		if(index === 20) clearInterval(interval)
+		console.log(index++)
+
+		/**
+		 * After delayed-duration, execute given function
+		 */
+		throttle(() => console.log('execution'), 1000, {executeOnCount: 7})
+	}, 200)
+
+	setTimeout(() => throttleTest(), 10000)
 }
 
 module.exports = Restrain
